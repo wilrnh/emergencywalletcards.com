b2a40f6e3f4f49e372331a7a03c1714ec5c06f53
Paramdeo Singh <paramdeo@paramdeo.com>
2021-11-13 05:19:45 -0400

    Autofocus cursor to first input field
    Utilizes the semantic [autofocus](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autofocus) attribute which works in most browsers, and will be standardized soon.

--- pages/getstarted.js
+++ pages/getstarted.js
@@ -690,7 +690,7 @@ export default function GetStarted() {
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="fullName">
                           Full Name
                         </label>
-                        <Field type="text" name="fullName" id="fullName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                        <Field type="text" name="fullName" id="fullName" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" autofocus/>
                         <ErrorMessage className="text-red-500 text-xs italic" name="fullName" component="div"/>
                       </div>
                     </div>
